2b0121517d9b98db552f38aaa062b8324ee1da3f
Storczyk <17184287+storczykorg@users.noreply.github.com>
2026-05-15 16:05:27 +0200

    fix(apphost): add healthchecks to both frontend and backend services

--- src/Storczyk.AppHost/AppHost.cs
+++ src/Storczyk.AppHost/AppHost.cs
@@ -7,7 +7,8 @@ var db = builder.AddPostgres("pg")
 var backendService = builder
     .AddProject<Projects.StorczykProdpol>("StorczykProdpol")
     .WithReference(db)
-    .WaitFor(db);
+    .WaitFor(db)
+    .WithHttpHealthCheck("api/status/health");
 //var workerService = builder.AddProject<Projects.StorczykWorker>("StorczykWorker")
 //    .WithReference(db)
 //    .WaitFor(db);
@@ -18,5 +19,6 @@ var frontend = builder.AddViteApp("StorczykFrontend",
     .WithEnvironment("BROWSER", "none")
     .WithEnvironment("VITE_API_URL", backendService.GetEndpoint("http"))
     .WithHttpEndpoint(env: "VITE_PORT")
+    .WithHttpHealthCheck("api/status/health")
     .WithExternalHttpEndpoints();
 builder.Build().Run();
