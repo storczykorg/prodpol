7b81c6dc60daf0113dd5f046a17312bff27699d2
Storczyk <17184287+storczykorg@users.noreply.github.com>
2026-05-15 21:48:31 +0200

    refactor: move database module to libraries

---
+++ lib/Storczyk.Prodpol.Core/Data/IRepository.fs
@@ -0,0 +1,25 @@
+﻿namespace Storczyk.Prodpol.Core.Data
+
+open System.Collections.Generic
+open System.Data
+open System.Threading
+open System.Threading.Tasks
+open FSharp.Control
+
+type RepoResult =
+    | Ok
+    | Error of DataException
+
+type IRepository<'TKey, 'TValue> =
+    interface
+        abstract GetAllAsync: token: CancellationToken -> AsyncSeq<'TValue>
+        abstract GetByIdAsync: id: 'Tkey -> Async<'TValue option>
+        abstract AddAsync: entity: 'TValue -> Async<RepoResult>
+        abstract UpdateAsync: key: 'TKey -> entity: 'TValue -> Async<RepoResult>
+        abstract DeleteAsync: key: 'TKey -> Async<RepoResult>
+    end
+
+type IEmployeesRepository =
+    interface
+        
+    end
---
+++ lib/Storczyk.Prodpol.Core/Models/Employee.fs
@@ -0,0 +1,16 @@
+﻿namespace Storczyk.Prodpol.Core.Models
+
+open System
+open System.ComponentModel.DataAnnotations.Schema
+
+[<Table("prodpol.employees")>]
+type Employee() =
+    member val Id = -1l with get, set
+    member val RoleId = -1 with get, set
+    member val Email = "" with get, set
+    member val NameFirst = "" with get, set
+    member val NameLast = "" with get, set
+    member val PhoneNumber = "" with get, set
+    member val PasswordHash: string option = None with get, set
+    member val CreatedAt = DateTime.UtcNow with get, set
+    member val Enabled = false with get, set
