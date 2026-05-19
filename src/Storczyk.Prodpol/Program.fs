5aff653242f17cb49ee767bf992c6e08b0ca02fc
Storczyk <17184287+storczykorg@users.noreply.github.com>
2026-05-19 19:32:01 +0200

    feat(Prodpol): add database seeding in dev environment and type mapping from LinqToDB annotations for Dapper
    Signed-off-by: Storczyk <17184287+storczykorg@users.noreply.github.com>

--- src/Storczyk.Prodpol/Program.fs
+++ src/Storczyk.Prodpol/Program.fs
@@ -2,15 +2,21 @@ namespace Storczyk.Prodpol
 
 #nowarn "20"
 
+open System
+open System.Linq
+open System.Reflection
+open Dapper
+open LinqToDB.Mapping
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 open Storczyk.Database.Services
+open Storczyk.Prodpol.Core.Models
 open Storczyk.Prodpol.Core.Services
 
 module Program =
-    let exitCode = 0
+    let mutable exitCode = 0
 
     let applyUpgrade (app: WebApplication) : bool =
         use scope = app.Services.CreateScope()
@@ -23,7 +29,32 @@ module Program =
             false
         else
             true
+    let applySeeding (app: WebApplication): bool =
+        use scope = app.Services.CreateScope()
+        let upgrader = app.Services.GetRequiredService<PostgresSeedUpgrader>()
 
+        let result = upgrader.Build().PerformUpgrade()
+
+        if not result.Successful then
+            app.Logger.LogCritical("Can't apply migration: ", result.Error)
+            false
+        else
+            true
+    
+    let mapProperty(_type: Type) (name: string) : PropertyInfo =
+        _type.GetProperties().FirstOrDefault(
+            fun prop ->
+                prop.GetCustomAttributes<ColumnAttribute>(true)
+                    .Any(fun attr -> attr.Name = name)
+                )
+    let addTypeMapping<'T>() =
+        SqlMapper.SetTypeMap(
+            typeof<'T>,
+            CustomPropertyTypeMap(
+                typeof<'T>,
+                mapProperty
+                )
+            )
     let ConfigureServices (builder: WebApplicationBuilder) =
         builder.AddPostgresUpgrader()
 
@@ -47,7 +78,11 @@ module Program =
 
     [<EntryPoint>]
     let main args =
-
+        DefaultTypeMap.MatchNamesWithUnderscores = true
+        addTypeMapping<EmployeeRead>()
+        addTypeMapping<Employee>()
+        addTypeMapping<EmployeeRole>()
+        
         let builder = WebApplication.CreateBuilder(args)
 
         ConfigureServices builder
@@ -55,9 +90,14 @@ module Program =
         let app = builder.Build()
 
         if not (applyUpgrade app) then
-            1
+            exitCode <- 1
+        if app.Environment.IsDevelopment() && not(applySeeding app) then
+            exitCode <- 2
         else
             MapApplication app
 
             app.Run()
-            exitCode
+        exitCode
+    
+
+        
